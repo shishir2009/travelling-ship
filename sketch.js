f3d37a6f49b869e87289befe6352c2a303dee6e7
shishir2009 <86182277+shishir2009@users.noreply.github.com>
2021-07-08 18:18:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,40 +1,38 @@
-
-var seaship, seaship_travelling, edges;
-var seaImage;
+var sea,ship;
+var seaImg,shipImg;
 
 function preload(){
-seaship_travelling = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
-seaImage = loadImage("Sea.png");
+  seaImg = loadImage("sea.png");
+  shipImg1 = loadAnimation("ship-1.png","ship-1.png",
+                            "ship-2.png","ship-1.png");
 }
 
 function setup(){
-  createCanvas(1400,1000);
+  createCanvas(400,400);
+  background("blue");
 
-  //creating ship
-  seaship=createSprite(200,200,1000,1000);
-  seaship.addAnimation("travelling", seaship_travelling);
-  edges=createEdgeSprites();
- seaship.scale=0.5;
-  seaship.x=50;
+  // Moving background
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
 
- sea=createSprite(200,180,400,400);
-  sea.addImage(seaImage);
-  sea.velocityX=-2;
+  
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
   
 }
 
 function draw() {
-  background("blue");
+  background(0);
+  sea.velocityX = -3;
 
-  console.log(seaship.x);
-
-  if(sea.x<0){
-    sea.x=sea.width/2;
-  }
   
-  seaship.velocityX = seaship.velocityX + 0.5;
- 
-seaship.collide(edges[3]);
-drawSprites();
-
+  //code to reset the background
+  if(sea.x < 0){
+    sea.x = sea.width/8;
+  }
+    
+  drawSprites();
 }
