ae412fc802c7100c7709cc13fec4a5b51f4c36ca
shishir2009 <86182277+shishir2009@users.noreply.github.com>
2021-07-15 18:51:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ var seaImg,shipImg;
 function preload(){
   seaImg = loadImage("sea.png");
   shipImg1 = loadAnimation("ship-1.png","ship-1.png",
-                            "ship-2.png","ship-1.png");
+                            "ship-3.png","ship-4.png");
 }
 
 function setup(){
